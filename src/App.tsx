9f6d95de6aaa6a3cdf553eac02b210554d343e0d
tebsss17 <137368444+tebsss17@users.noreply.github.com>
2026-09-08 13:24:19 +0800

    library set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import { AdIcon } from 'lucide-react'
 
 function App() {
   return (
     <>
-     
+      <div>sup</div>
+      <AdIcon className='size-10' />
+      <p className='text-blue-400'>sdsd</p>
     </>
   )
 }
